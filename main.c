4c439ed1fbb78de40e5ab60c43c4049efcb3da06
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-11-12 15:47:06 +0900

    03

--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char *pc;
-	int *pi;
+	char buffer[8];
 	double *pd;
-	
-	pc=(char*)10000;
-	pi=(int*)10000;
-	pd=(double*)10000;
-	printf("befor increase : %d %d %d\n",pc,pi,pd);
-	++pc;
-	++pi;
-	++pd;
-	printf("after increase :%d %d %d\n",pc,pi,pd);
+	int *pi;
+	pd=(double*)buffer;
+	*pd=3.14;
+	printf("%f\n",*(double*)buffer);
+	pi = (int*)buffer;
+	*pi =123;
+	*(pi+1) =456;
+	printf("%d %d\n",*(int*)buffer,*((int*)buffer+1));
 	
 	return 0;
 }
+
